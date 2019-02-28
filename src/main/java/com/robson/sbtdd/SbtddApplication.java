4471a6fbe473012c5b42d0cb0fef4eac2ad6e894
robsonsilv4 <robsonsilv410@gmail.com>
2019-02-28 20:46:44 -0300

    Projeto inicial

---
+++ src/main/java/com/robson/sbtdd/SbtddApplication.java
@@ -0,0 +1,13 @@
+package com.robson.sbtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtddApplication.class, args);
+	}
+
+}
